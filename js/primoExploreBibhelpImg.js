0304f72de36f3e250c19a366ca67816b1997c718
mjmss <75858684+michelaj-test@users.noreply.github.com>
2024-11-30 11:03:06 +0100

    File precedentemente creati e incollati

---
+++ js/primoExploreBibhelpImg.js
@@ -0,0 +1,9 @@
+/*
+ * adding "BiblioHelp" linked image at the right edge of the page
+	sostituire VU2 con primo [vid]
+ */
+
+app.component('prmSilentLoginAfter', {
+	bindings: {},
+	template: `<div class="bibliohelp-tab"><a title="BiblioHELP" href="https://unimi.libanswers.com/"><img alt="BiblioHELP" src="/discovery/custom/VU2/img/bibliohelp.png"></a></div>`
+});
